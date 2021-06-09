fda7fda1a9a0d79793b8bd82d594a21b20e12022
jim48949 <panjin48949@gmail.com>
2021-06-08 23:05:58 -0700

    Add UI; Add ability to parse existed html file

--- driver.py
+++ driver.py
@@ -1,11 +1,45 @@
 import crawler
+import parse
 import os
 
-# Load seeds
-URLseed = open('seeds.txt', 'r')
-URLS = [url for url in URLseed.read().split()]
+def crawling():
+    # Load seeds
+    URLseed = open('seeds.txt', 'r')
+    URLS = [url for url in URLseed.read().split()]
+    pages = int(input('Enter number of pages that you want to crawl: '))
 
-pages = int(input('Enter number of pages that you want to crawl: '))
+    # Call the crawler
+    crawler.crawler(URLS,pages)
 
-# Call the crawler
-crawler.crawler(URLS,pages)
+def parsing():
+    file = input('Enter the htmlFile that you want to parse(including the path if not in the same directory): ')
+    parse.parse(file)
+
+# UI
+print('Welcome to Blablabla\'s crawer\n')
+
+while True:
+    print('Choose one of the options below to proceed:')
+    print('1. Call the crawler')
+    print('2. Display all the seeds (WIP)')
+    print('3. Add seed(s) into seeds.txt (WIP)')
+    print('4. Parse existed html')
+    print('0. Quit')
+    choice = int(input('Enter your choice: '))
+    print('\n')
+
+    if choice == 1:
+        # Crawl
+        crawling()
+    elif choice == 2:
+        # 2
+        print('2')
+    elif choice == 3:
+        # 3
+        print('3')
+    elif choice == 4:
+        # Parse html
+        parsing()
+    elif choice == 0:
+        break
+    
---
+++ parse.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+
+def parse(file):
+    f = open(file,'r',encoding='utf-8')
+    #page = f.read()
+    #print(page)
+    soup = BeautifulSoup(f,'html.parser')
+    # Get all text from body tag
+    tag = soup.body
+    # Output to body.txt
+    tempTxt = open('body.txt','w',encoding='utf-8')
+    for string in tag.strings:
+        if not string.isspace():
+            tempTxt.write(string)
+    
+    tempTxt.close()
+    print('Parsing done, all text in body tag will be stored in body.txt\n')
+        
