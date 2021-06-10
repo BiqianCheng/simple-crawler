462c69ecd9cbf678840acd9b9c42f02695ebed5a
jim48949 <panjin48949@gmail.com>
2021-06-10 11:28:21 -0700

    Make changes to option 2 and 3

--- driver.py
+++ driver.py
@@ -1,8 +1,6 @@
 import crawler
 import parse
 import os
-from urllib.parse import urlparse
-
 
 def crawling():
     # Load seeds
@@ -11,14 +9,64 @@ def crawling():
     pages = int(input('Enter number of pages that you want to crawl: '))
 
     # Call the crawler
-    crawler.crawler(URLS, pages)
-
+    crawler.crawler(URLS,pages)
 
 def parsing():
-    file = input(
-        'Enter the htmlFile that you want to parse(including the path if not in the same directory): ')
+    file = input('Enter the htmlFile that you want to parse(including the path if not in the same directory): ')
     parse.parse(file)
+    
+def display():
+    try:
+        print('Printing all existing seeds: \n')
+        file = open('seeds.txt','r')
+        counter = 1
+        for line in file.readlines():
+            print(counter,'.',line)
+            counter += 1
+        print('\n')
+    except:
+        print('Could not open seeds.txt!')
 
+def changeSeeds():
+    while True:
+        print('Would you like to:')
+        print('1. Add one seed into seeds.txt')
+        print('2. Delete one seed from seeds.txt')
+        print('3. Go back to main menu')
+        seedCho = int(input('Enter your choice: '))
+        print('\n')
+        if seedCho == 1:
+            file = open('seeds.txt','a')
+            newSeed = input('Enter the seed: ')
+            if 'https://' or 'http://' not in newSeed:
+                newSeed = 'https://' + newSeed
+            if '.edu' not in newSeed:
+                print('\nWarning: This is not a edu page, do you still wish to continue? (y/n)')
+                cont = input('')
+                if cont == 'n':
+                    print('Got it! This seed will not be saved into seeds.txt\n')
+                    continue
+                else:
+                    print('Got it! Seed will be added into seeds.txt')
+                    print('Disclaimer: This crawler is not designed for non edu page, do it at your own risk!\n')
+            file.write('\n')
+            file.write(newSeed)
+            file.close()
+            print('Seed has been added into seeds.txt!\n')
+        elif seedCho == 2:
+            rfile = open('seeds.txt','r')
+            lines = rfile.readlines()
+            rfile.close()
+            display()
+            delSeed = input('Enter the seed that you want to delete: ')
+            wfile = open('seeds.txt','w')
+            for line in lines:
+                if line != delSeed and not line.isspace() :
+                    wfile.write(line)
+            wfile.close()
+            print('Seed has been deleted!\n')
+        elif seedCho == 3:
+            break
 
 # UI
 print('Welcome to Blablabla\'s crawer\n')
@@ -26,8 +74,8 @@ print('Welcome to Blablabla\'s crawer\n')
 while True:
     print('Choose one of the options below to proceed:')
     print('1. Call the crawler')
-    print('2. Display all the seeds (WIP)')
-    print('3. Add seed(s) into seeds.txt (WIP)')
+    print('2. Display all the seeds')
+    print('3. Make changes to seeds.txt')
     print('4. Parse existed html')
     print('0. Quit')
     choice = int(input('Enter your choice: '))
@@ -38,24 +86,13 @@ while True:
         crawling()
     elif choice == 2:
         # 2
-        with open("seeds.txt", "r") as f:
-            print("Printing top 10 of the seeds:\n")
-            print(f.read())
-            print('\n')
+        display()
     elif choice == 3:
         # 3
-        while True:
-            seed = input("Enter your seed url:\n")
-            if(urlparse(seed).scheme):
-                with open("seeds.txt", "a") as f:
-                    print(urlparse(seed).geturl())
-                    f.write(urlparse(seed).geturl() + "\n")
-                    break
-            else:
-                print("Invalid URL")
-
+        changeSeeds()
     elif choice == 4:
         # Parse html
         parsing()
     elif choice == 0:
         break
+    
